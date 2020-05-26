f49967e32c2691045b53803375c3349d2601e526
Luis Novoa <luis.matte.novoa@gmail.com>
2020-05-26 18:40:27 -0400

    Creates Home model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_203231) do
+ActiveRecord::Schema.define(version: 2020_05_26_223901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homes", force: :cascade do |t|
+    t.string "title"
+    t.string "address"
+    t.string "city"
+    t.string "country"
+    t.decimal "rent"
+    t.string "room_type"
+    t.text "more_info"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_homes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_05_23_203231) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "homes", "users"
 end
